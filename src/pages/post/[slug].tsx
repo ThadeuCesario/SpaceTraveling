118f5ca6712c1c2b323c558afa42cbdf2ca9ef21
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-21 07:37:10 -0300

    implementing getStaticPaths and treating fallback case

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import { format } from 'date-fns';
+import Prismic from '@prismicio/client';
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
-import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './post.module.scss';
 
@@ -29,65 +30,68 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  const router = useRouter();
   const dateFormated = String(
     format(new Date('2021-03-25T19:25:28+0000'), 'dd MMM yyyy')
   ).toLowerCase();
 
-  return (
-    <>
-      {post ? (
-        <section>
-          <img
-            src={post.data.banner.url}
-            alt="banner"
-            className={styles.banner}
-          />
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
 
-          <div className={styles.content}>
-            <h1>{post.data.title}</h1>
-            <div className={styles.postDetails}>
-              <div>
-                <FiCalendar color="#bbbbbb" />
-                <span>{dateFormated}</span>
-              </div>
-              <div>
-                <FiUser color="#bbbbbb" />
-                <span>{post.data.author}</span>
-              </div>
-              <div>
-                <FiClock color="#bbbbbb" />
-                <span>4 min</span>
-              </div>
-            </div>
-            <div className={styles.containerContent}>
-              {post.data.content.map(info => (
-                <div key={Math.random()}>
-                  <h1>{info.heading}</h1>
-                  <div>
-                    {info.body.map(task => (
-                      <p key={Math.random()}>{task.text}</p>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
+  return (
+    <section>
+      <img src={post.data.banner.url} alt="banner" className={styles.banner} />
+
+      <div className={styles.content}>
+        <h1>{post.data.title}</h1>
+        <div className={styles.postDetails}>
+          <div>
+            <FiCalendar color="#bbbbbb" />
+            <span>{dateFormated}</span>
           </div>
-        </section>
-      ) : (
-        <div>Carregando...</div>
-      )}
-    </>
+          <div>
+            <FiUser color="#bbbbbb" />
+            <span>{post.data.author}</span>
+          </div>
+          <div>
+            <FiClock color="#bbbbbb" />
+            <span>4 min</span>
+          </div>
+        </div>
+        <div className={styles.containerContent}>
+          {post.data.content.map(info => (
+            <div key={Math.random()}>
+              <h1>{info.heading}</h1>
+              <div>
+                {info.body.map(task => (
+                  <p key={Math.random()}>{task.text}</p>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // console.log('getStaticPaths', props);
-  // const prismic = getPrismicClient();
-  // const posts = await prismic.query(TODO);
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'post'),
+  ]);
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
 
   return {
-    paths: [],
-    fallback: true,
+    paths,
+    fallback: false,
   };
 };
 
