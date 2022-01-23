f28dbdf4febc57da52b3a66482a635a8070a43fc
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-23 15:31:21 -0300

    importing google fonts - inter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,24 @@
-import Document from 'next/document';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
