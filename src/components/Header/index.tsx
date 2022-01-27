7e0be9ca1b03f4dad53f2ceb71738ffa22f346dc
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-27 08:17:18 -0300

    implementing header component in SpaceTraveling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,9 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.png" alt="logo" />
+    </header>
+  );
 }
