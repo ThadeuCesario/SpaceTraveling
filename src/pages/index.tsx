9ed91c397aaa482679fa4610d6fe355312efafe9
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-01-29 08:17:41 -0300

    implementing header component in SpaceTraveling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticProps } from 'next';
-
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,8 +28,28 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
-  return <h1>Home</h1>;
+// {
+//   next_page: ,
+//   results: [
+//     {
+//       uid?: string;
+//       first_publication_date: string | null;
+//       data: {
+//         title: string;
+//         subtitle: string;
+//         author: string;
+//       };
+//     }
+//   ]
+// }
+
+export default function Home({ postsPagination }: HomeProps) {
+  console.log(postsPagination);
+  return (
+    <>
+      <div />
+    </>
+  );
 }
 
 export const getStaticPaths = () => {
@@ -39,11 +61,29 @@ export const getStaticPaths = () => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const postsResponse = await prismic.query([
-    Prismic.predicates.at('document.type', 'post'),
-  ]);
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 1,
+    }
+  );
 
-  console.log('postsResponse', postsResponse);
+  console.log(postsResponse);
+
+  // const postsPagination = postsResponse.results.map(post => {
+  //   // return {
+  //   //   slug: post.uid,
+  //   //   title: RichText.asText(post.data.title),
+  //   //   subtitle: RichText.asText(post.data.subtitle),
+  //   //   author: RichText.asText(post.data.author),
+  //   //   updatedAt: format(new Date(post.last_publication_date), "dd 'de' YYYY"),
+  //   // };
+  //   console.log('post', post);
+  //   return {
+  //     next_page:
+  //   };
+  // });
 
   return {
     props: {},
