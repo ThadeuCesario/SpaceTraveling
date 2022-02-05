5a89ca8fc68ad272b750e052dba3fbcc2a57b1c3
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-02-05 19:22:13 -0300

    redirecting to home when click in logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.png" alt="logo" />
+      <Link href="/">
+        <a>
+          <img src="/logo.png" alt="logo" />
+        </a>
+      </Link>
     </header>
   );
 }
