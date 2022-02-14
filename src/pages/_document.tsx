771c8b613f18bc007a70ee208e2badfb549cedba
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2022-02-14 20:14:53 -0300

    updating document to preview element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            defer
+            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=spacetravelingth"
+          />
         </Head>
         <body>
           <Main />
